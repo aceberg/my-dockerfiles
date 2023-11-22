6c23b83069887df76d8ada71a0f78f344846413c
aceberg <1502200+aceberg@users.noreply.github.com>
2023-11-23 04:14:57 +0700

    invidious-postgres

---
+++ invidious-postgres/README.md
@@ -0,0 +1,6 @@
+# invidious-postgres
+
+
+
+
+
---
+++ invidious-postgres/docker-entrypoint-initdb.d/init-invidious-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Runing init script for INVIDIOUS"
+
+set -eou pipefail
+
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channels.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channel_videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/users.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/session_ids.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/nonces.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/annotations.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlists.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlist_videos.sql
